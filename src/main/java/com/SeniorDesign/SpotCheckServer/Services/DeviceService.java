d045f42586bc1963a149bc82061f55f460e40438
bunkerrh <ryanhbunker@gmail.com>
2019-09-16 22:31:07 -0400

    Made changes to DeviceController and DeviceService

--- src/main/java/com/SeniorDesign/SpotCheckServer/Services/DeviceService.java
+++ src/main/java/com/SeniorDesign/SpotCheckServer/Services/DeviceService.java
@@ -1,8 +1,14 @@
 package com.SeniorDesign.SpotCheckServer.Services;
 
+import com.SeniorDesign.SpotCheckServer.Controllers.DeviceController;
 import com.SeniorDesign.SpotCheckServer.Models.Device;
 import com.SeniorDesign.SpotCheckServer.Repositorys.DeviceRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -12,10 +18,30 @@ public class DeviceService
     @Autowired
     DeviceRepository deviceRepository;
 
+    Logger log = LoggerFactory.getLogger(DeviceService.class);
+
+
+
     public List<Device> getAllDevices()
     {
         return deviceRepository.getDevices();
     }
 
-    public String updateDevice(){ return "This is a test"; }
+    public ResponseEntity updateDevice(String requestDto)
+    {
+        try
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            Device device = mapper.readValue(requestDto, Device.class);
+        }
+        catch (Exception ex)
+        {
+            log.error("Error processing update device");
+            log.error(ex.getLocalizedMessage());
+            return new ResponseEntity("updateDevice", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("updateDevice", HttpStatus.OK);
+
+    }
 }
